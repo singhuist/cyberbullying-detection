aec298d43531f61ac5dcb5741d116ed14ebca049
athenexcalibur <s.soumya1997@gmail.com>
2018-10-27 15:36:55 +0100

    plots graphs for val and train acc and loss

--- datascripts/preprocess.py
+++ datascripts/preprocess.py
@@ -112,11 +112,6 @@ train_label = isbully[:30000]
 test_data = ans[30000:]
 test_label = isbully[30000:]
 
-print(train_data[100:110], "yo")
-print(train_label[100:110], "gah")
-print(test_data[100:110], "errr")
-print(test_label[100:110], "eeeh")
-
 train_data = keras.preprocessing.sequence.pad_sequences(train_data,
                                                         value=0,
                                                         padding='post',
@@ -156,8 +151,8 @@ partial_y_train = train_label[200:30000]
 ##train the model
 history = model.fit(partial_x_train,
                     partial_y_train,
-                    epochs=20,
-                    batch_size=512,
+                    epochs=50,
+                    batch_size=100,
                     validation_data=(x_val, y_val),
                     verbose=1)
 
@@ -165,3 +160,39 @@ history = model.fit(partial_x_train,
 results = model.evaluate(test_data, test_label)
 
 print(results)
+
+history_dict = history.history
+history_dict.keys()
+
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+plt.clf()   # clear figure
+acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.show()
