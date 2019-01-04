d61cee5a20b62e69264550649141399b280e5a31
Soumya Singh <soumya.singh@durham.ac.uk>
2019-01-04 13:22:05 +0000

    k-fold cross validation,plots graphs

--- gloveWorks.py
+++ gloveWorks.py
@@ -5,12 +5,18 @@ import tensorflow as tf
 import data_preprocess
 import embeddings
 import nn_models
+from sklearn.model_selection import ShuffleSplit
+import math
+import matplotlib.pyplot as plt
 
 
+print("Loading Data ...")
 data_source = 'data/hatebase_labeled_data.csv'
+
+print("preprocessing Data ...")
 data, classification, severity = data_preprocess.parseFile(data_source)
-docs = data[:50]
-labels = array(classification[:50])
+docs = data
+labels = array(classification)
 sequences, padlength, wordIndex = embeddings.simpleEncode(data)
 
 # prepare tokenizer
@@ -19,12 +25,13 @@ t.fit_on_texts(docs)
 vocab_size = len(t.word_index) + 1
 # integer encode the documents
 encoded_docs = t.texts_to_sequences(docs)
-# pad documents to a max length of 4 words
+# pad documents
 padded_docs = tf.keras.preprocessing.sequence.pad_sequences(encoded_docs, maxlen=padlength, padding='post')
 
+print("Prepare Embeddings ...")
 # load the whole embedding into memory
 embeddings_index = dict()
-f = open('GloVe/glove.6B.100d.txt',encoding="utf8")
+f = open('GloVe/glove.6B.50d.txt',encoding="utf8")
 for line in f:
 	values = line.split()
 	word = values[0]
@@ -34,33 +41,86 @@ f.close()
 
 print('Loaded %s word vectors.' % len(embeddings_index))
 
+print("preparing embedding matrices ...")
 # create a weight matrix for words in training docs
-embedding_matrix = zeros((vocab_size, 100))
+embedding_matrix = zeros((vocab_size, 50))
 for word, i in t.word_index.items():
 	embedding_vector = embeddings_index.get(word)
 	if embedding_vector is not None:
 		embedding_matrix[i] = embedding_vector
+	else:
+		embedding_matrix[i] = array([0 for x in range(50)])
 
-e = tf.keras.layers.Embedding(vocab_size, 100, weights=[embedding_matrix], input_length=padlength, trainable=False)
 
-trainX = padded_docs[:35]
-trainY = labels[:35]
-valX = padded_docs[35:40]
-valY = labels[35:40]
-testX = padded_docs[40:]
-testY = labels[40:]
+e = tf.keras.layers.Embedding(vocab_size, 50, weights=[embedding_matrix], input_length=padlength, trainable=False)
 
-##history = nn_models.MultiLayerPerceptron(trainX,trainY,valX,valY,testX,testY,e)
-history = nn_models.GatedRecurrentUnit(trainX,trainY,valX,valY,testX,testY,e)
+print("Shuffling sets....")
+# SHUFFLE DATA AND SET UP CROSS-VALIDATION
+shuffdata = ShuffleSplit(n_splits=5, test_size=0.2, train_size=0.8)
 
-'''history_dict = history.history
-history_dict.keys()
+train_accuracies = [] #stores the average of different cross validations
+val_accuracies = [] #stores the average of different cross validations
+train_losses = [] #stores the average of different cross validations
+val_losses = [] #stores the average of different cross validations
 
-# PLOT GRAPHS
+for train_index, test_index in shuffdata.split(padded_docs,labels):
 
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+	trainX = []
+	trainY = []
 
-print("ACCURACY IS: ",acc)'''
+	testX = []
+	testY = []
+
+	for tr in train_index:
+		trainX.append(padded_docs[tr])
+		trainY.append(labels[tr])
+
+	for ts in test_index:
+		testX.append(padded_docs[ts])
+		testY.append(labels[ts])
+
+	# VALIDATION DATA
+	valsize = len(trainX) - math.floor(0.2 * len(trainX))
+	valX = array(trainX[valsize:len(trainX)])
+	valY = array(trainY[valsize:len(trainX)])
+	trainX = array(trainX[:valsize])
+	trainY = array(trainY[:valsize])
+
+	testX = array(testX)
+	testY = array(testY)
+
+	print("Creating and Training Model ...")
+	history = nn_models.GatedRecurrentUnit(trainX,trainY,valX,valY,testX,testY,e)
+
+	history_dict = history.history
+	history_dict.keys()
+
+	# PLOT GRAPHS
+	print("Plotting graphs ...")
+	acc = history.history['acc']
+	val_acc = history.history['val_acc']
+	loss = history.history['loss']
+	val_loss = history.history['val_loss']
+	epochs = range(1, len(acc) + 1)
+
+	'''plt.plot(epochs, acc, 'bo', label='Training acc')
+	plt.plot(epochs, val_acc, 'b', label='Validation acc')
+	plt.title('Training and validation accuracy')
+	plt.xlabel('Epochs')
+	plt.ylabel('Accuracy')
+	plt.legend()
+	plt.show()'''
+
+	train_accuracies.append(sum(acc) / len(acc))
+	val_accuracies.append(sum(val_acc) / len(val_acc))
+
+
+#OVERALL AVG MODEL ACCURACIES
+kfold = range(1, len(train_accuracies) + 1)
+plt.plot(kfold, train_accuracies, 'bo', label='Training acc')
+plt.plot(kfold, val_accuracies, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('K-Fold')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
