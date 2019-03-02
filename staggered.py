a091e2a13e3925d11d636d2fb7eb90c0626f6066
athenexcalibur <s.soumya1997@gmail.com>
2019-03-02 23:42:12 +0000

    at least it works

--- staggered.py
+++ staggered.py
@@ -1,41 +1,47 @@
-from numpy import array
-from numpy import asarray
-from numpy import zeros
+from numpy import array, asarray, zeros
 import tensorflow as tf
-import data_preprocess
-import embeddings
-import nn_models
+import data_preprocess, embeddings
+import nn_models, multi_models
 from sklearn.model_selection import ShuffleSplit
 import math
 import matplotlib.pyplot as plt
 
 
 print("Loading Data ...")
-data_source = 'data/hatebase_labeled_data.csv'
-no_bully_source = 'data/formspring_data.csv'
+
+data_source = 'data/twitter_data.pkl'
 
 print("preprocessing Data ...")
-data_bi, classification_bi = data_preprocess.biClass(data_source)
-data_mult, classification_mult = data_preprocess.mult_only(data_source)
-data_nb, class_nb = data_preprocess.noBully(no_bully_source)
-docs_bi = data_bi + data_nb
-docs_mult = data_mult
-labels_bi = array(classification_bi + class_nb)
-labels_mult = array(classification_mult)
-sequences_bi, padlength_bi = embeddings.simpleEncode(data_bi)
-sequences_mult, padlength_mult = embeddings.simpleEncode(data_mult)
+
+data, classification = data_preprocess.pickleData(data_source)
+mult_data, mult_class = data_preprocess.pickleData_multi(data_source)
+#bully_data, bully_class = data_preprocess.pickleData_multi(data_source)
+
+#print(classification[:30])
+#print(mult_class[:30])
+
+del mult_data
+
+docs = data
+labels = array(classification)
+mult_labels = array(mult_class)
+#bully_labels = array(bully_class)
+
+sequences, padlength = embeddings.simpleEncode(data)
+
+del data
+del classification
+del mult_class
 
 # prepare tokenizer
 t = tf.keras.preprocessing.text.Tokenizer()
-t.fit_on_texts(docs_bi)
-t.fit_on_texts(docs_mult)
+t.fit_on_texts(docs)
+
 vocab_size = len(t.word_index) + 1
 # integer encode the documents
-encoded_docs_bi = t.texts_to_sequences(docs_bi)
-encoded_docs_mult = t.texts_to_sequences(docs_mult)
+encoded_docs = t.texts_to_sequences(docs)
 # pad documents
-padded_docs_bi = tf.keras.preprocessing.sequence.pad_sequences(encoded_docs_bi, maxlen=padlength_bi, padding='post')
-padded_docs_mult = tf.keras.preprocessing.sequence.pad_sequences(encoded_docs_mult, maxlen=padlength_mult, padding='post')
+padded_docs = tf.keras.preprocessing.sequence.pad_sequences(encoded_docs, maxlen=padlength, padding='post')
 
 print("Prepare Embeddings ...")
 # load the whole embedding into memory
@@ -61,79 +67,91 @@ for word, i in t.word_index.items():
 		embedding_matrix[i] = array([0 for x in range(50)])
 
 
-e_bi = tf.keras.layers.Embedding(vocab_size, 50, weights=[embedding_matrix], input_length=padlength_bi, trainable=False)
-e_mult = tf.keras.layers.Embedding(vocab_size, 50, weights=[embedding_matrix], input_length=padlength_mult, trainable=False)
+e = tf.keras.layers.Embedding(vocab_size, 50, weights=[embedding_matrix], input_length=padlength, trainable=True)
 
 print("Shuffling sets....")
 # SHUFFLE DATA AND SET UP CROSS-VALIDATION
-shuffdata = ShuffleSplit(n_splits=10, test_size=0.2, train_size=0.8)
+shuffdata = ShuffleSplit(n_splits=3, test_size=0.2, train_size=0.8)
 
 train_accuracies = [] #stores the average of different cross validations
 val_accuracies = [] #stores the average of different cross validations
 train_losses = [] #stores the average of different cross validations
 val_losses = [] #stores the average of different cross validations
 
-for train_index, test_index in shuffdata.split(padded_docs_bi,labels_bi):
+for train_index, test_index in shuffdata.split(padded_docs,labels):
 
 	trainX = []
 	trainY = []
+
+	trainX_mult = []
+	trainY_mult = []
 
 	testX = []
 	testY = []
 
 	for tr in train_index:
-		trainX.append(padded_docs_bi[tr])
-		trainY.append(labels_bi[tr])
 
+		trainX.append(padded_docs[tr])
+		trainY.append(labels[tr])
+
+		
+		if labels[tr] == 1:
+			trainX_mult.append(padded_docs[tr])
+			trainY_mult.append(mult_labels[tr])
+
+			
+	multtestY = []
 	for ts in test_index:
-		testX.append(padded_docs_bi[ts])
-		testY.append(labels_bi[ts])
+		testX.append(padded_docs[ts])
+		testY.append(labels[ts])
+		multtestY.append(mult_labels[ts])
 
 	# VALIDATION DATA
 	valsize = len(trainX) - math.floor(0.2 * len(trainX))
 	valX = array(trainX[valsize:len(trainX)])
 	valY = array(trainY[valsize:len(trainX)])
+
+	valsize_mult = len(trainX_mult) - math.floor(0.2 * len(trainX_mult))
+	valX_mult = array(trainX_mult[valsize_mult:len(trainX_mult)])
+	valY_mult = array(trainY_mult[valsize_mult:len(trainX_mult)])
+
 	trainX = array(trainX[:valsize])
 	trainY = array(trainY[:valsize])
+	
+	trainX_mult = array(trainX_mult[:valsize_mult])
+	trainY_mult = array(trainY_mult[:valsize_mult])
 
 	testX = array(testX)
 	testY = array(testY)
 
-	print("Creating and Training Model ...")
-	bi_history,bi_prediction = nn_models.GatedRecurrentUnit(trainX,trainY,valX,valY,testX,testY,e_bi)
+	
+	print("Creating and Training Bi-Classification Model ...")
+	#bi_history,bi_prediction = nn_models.GatedRecurrentUnit(trainX,trainY,valX,valY,testX,testY,e)
+	bi_history,bi_prediction = nn_models.GatedRecurrentUnit(trainX,trainY,valX,valY,testX,testY,e)
 
-	mult_testX = []
-	mult_testY = []
+	testX_mult = []
+	testY_mult = []
 
 	for p in range(len(bi_prediction)):
 		if bi_prediction[p] == 1:
-			mult_testX.append(testX[p])
-			mult_testY.append(testY[p])
+			testX_mult.append(testX[p])
+			testY_mult.append(multtestY[p])
 
-	mult_testX = array(mult_testX)
-	mult_testY = array(mult_testY)
+	print("Obtained multi-class training data")
 
-	print(len(mult_testX),":",len(mult_testY))
-
-	mult_trainX = padded_docs_mult
-	mult_trainY = labels_mult
-
-	# VALIDATION DATA
-	valsize = len(mult_trainX) - math.floor(0.2 * len(mult_trainX))
-	mult_valX = array(mult_trainX[valsize:len(mult_trainX)])
-	mult_valY = array(mult_trainY[valsize:len(mult_trainX)])
-	mult_trainX = array(mult_trainX[:valsize])
-	mult_trainY = array(mult_trainY[:valsize])
-
-	print(len(mult_trainX),":",len(mult_trainY))
-	print(len(mult_valX),":",len(mult_valY))
+	testX_mult = array(testX_mult)
+	testY_mult = array(testY_mult)
 
 
-	print("Creating and Training Model ...")
-	history,mult_prediction = nn_models.GatedRecurrentUnit(mult_trainX,mult_trainY,mult_valX,mult_valY,mult_testX,mult_testY,e_mult)
+	print(set(valY_mult))
+	print(set(testY_mult))
+
+	print("Creating and Training Multi-Classification Model ...")
+	history,mult_prediction = multi_models.gru_multi(trainX_mult,trainY_mult,valX_mult,valY_mult,testX_mult,testY_mult,e)
+	
 
 
-	history_dict = history.history
+	'''history_dict = history.history
 	history_dict.keys()
 
 	# PLOT GRAPHS
@@ -153,11 +171,11 @@ for train_index, test_index in shuffdata.split(padded_docs_bi,labels_bi):
 	plt.show()
 
 	train_accuracies.append(sum(acc) / len(acc))
-	val_accuracies.append(sum(val_acc) / len(val_acc))
+	val_accuracies.append(sum(val_acc) / len(val_acc))'''
 
 
 #OVERALL AVG MODEL ACCURACIES
-kfold = range(1, len(train_accuracies) + 1)
+'''kfold = range(1, len(train_accuracies) + 1)
 plt.plot(kfold, train_accuracies, 'bo', label='Training acc')
 plt.plot(kfold, val_accuracies, 'b', label='Validation acc')
 plt.title('Training and validation accuracy')
@@ -165,7 +183,7 @@ plt.xlabel('K-Fold')
 plt.ylabel('Accuracy')
 plt.legend()
 
-plt.show()
+plt.show()'''
 
 
 
